4052e8d860801283a4f561c14af6b678fdee00f7
atomjump <peter@atomjump.com>
2022-11-07 11:44:41 +1300

    trimming in place

--- trim-messages.php
+++ trim-messages.php
@@ -2,21 +2,20 @@
 
 	/*
 	
-	Query for all aged message forums based off the tbl_layer.date_to_decay values. Note: most
-	forums will have a NULL entry here, which means they don't age and should be ignored.
+	Trim message forums that are flagged to be trimmed from the oldest first. 
 	This will be called hourly on a regular cronjob.
 	
-	0 * * * *       /usr/bin/php /your_server_path/api/plugins/remove_aged/index.php
+	0 * * * *       /usr/bin/php /your_server_path/api/plugins/overflow/index.php
 	
-	1. Loop through each forum, query for all messages from that forum.
-	2. Loop through each message from that forum and search individually within that message for
+	2. Loop through each message from the bototm of that forum and search individually within that message for
 	images that are held on this server. They will start with the config.json JSON entry uploads.vendor.imageURL
 	if images are uploaded via AmazonAWS (/digitalocean), or with the current server URL /images/im/ if uploaded
 	to the same server.
 	3. Delete the image from using the AmazonAWS API or the local file-system unlink();
 	4. Delete the message
-	5. Repeat for all messages in forum. 
-	6. Delete the forum.
+	5. Repeat for all messages in the list at the bottom of the forum. 
+	
+	TODO: archiving old messages
 	
 	*/
 
