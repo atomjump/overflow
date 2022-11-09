ac66ef740bc97b9385fac07a3bd75f157a181e95
atomjump <peter@atomjump.com>
2022-11-09 13:26:18 +1300

    constraints added on adjusting size

--- index.php
+++ index.php
@@ -362,7 +362,7 @@
 				    		}
 				    	} else {
 				    		//Not authorised to set this new value.
-				    		$new_message = "Sorry, you cannot decrease the overflow count to below " . $max_messages . " messages, or set this above a maximum of " . $this->max_user_set_limit . " messages. You can contact your Admin user to request this change, however. " . $overflow_config ['contactAdminToRemoveLimits'];				    	
+				    		$new_message = "Sorry, you cannot decrease the overflow count to below " . $max_messages . " messages, or set this above a maximum of " . $this->max_user_set_limit . " messages. You can contact your System Admin to request this change, however. " . $overflow_config ['contactAdminToRemoveLimits'];				    	
 				    	}
 				      } else {
 				      	error_log("uc_message = " . $uc_message .  " strpos result:" . strpos($uc_message, "UNLIMITED"));		//TESTING
